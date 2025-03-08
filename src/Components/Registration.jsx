d24811cc7497fbd86126a3dcafc4529fa2c1364f
Aryan Gidde <giddearyan222@gmail.com>
2025-03-09 00:17:44 +0530

    Url

--- src/Components/Registration.jsx
+++ src/Components/Registration.jsx
@@ -7,7 +7,7 @@ import toast from "react-hot-toast";
 import axios from "axios";
 
 const Registration = () => {
-  const url = "http://localhost:5000";
+  const url = process.env.BACKEND_URL;
   const [curr, setCurr] = useState("Register");
   const { setAuthUser } = useAuthContext();
   const [role, setRole] = useState("user"); // ✅ New state for role (user/admin)
