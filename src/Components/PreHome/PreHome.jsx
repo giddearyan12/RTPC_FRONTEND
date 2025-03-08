a06cdc1c1dbb13527547dd1d4cede6bc197d9767
giddearyan12 <140741080+giddearyan12@users.noreply.github.com>
2025-03-09 00:23:32 +0530

    Update PreHome.jsx

--- src/Components/PreHome/PreHome.jsx
+++ src/Components/PreHome/PreHome.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./prehome.css";
+import "./PreHome.css";
 import { useNavigate } from "react-router-dom";
 import PreHomeImg from "../../assets/PreHomeImg.png";
 import ServiceCard from "./ServiceCard.jsx";
